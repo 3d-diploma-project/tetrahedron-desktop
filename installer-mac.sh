d88620f0eef67da8bc1d94671396314e82b487e8
mborodin <m.borodin.1999@gmail.com>
2024-12-23 22:45:48 +0100

    TET-161 updated project version

--- installer-mac.sh
+++ installer-mac.sh
@@ -1,11 +1,11 @@
 mkdir tmp tmp/app
-cp target/tetrahedron-desktop-1.0-SNAPSHOT-jar-with-dependencies.jar tmp/app/Tetrahedron.jar
+cp target/tetrahedron-desktop-1.0.0-SNAPSHOT-jar-with-dependencies.jar tmp/app/Tetrahedron.jar
 
 jlink --strip-debug --no-man-pages --no-header-files --compress zip-6 \
   --add-modules java.base,java.scripting,java.desktop,jdk.unsupported,jdk.unsupported.desktop --output tmp/jre
 
 jpackage --input tmp/app --main-jar Tetrahedron.jar --main-class org.cmps.tetrahedron.Tetrahedron --runtime-image tmp/jre \
-  --name Tetrahedron --vendor "CMPS, KhPI" --icon src/main/resources/logo.icns \
+  --name Tetrahedron --vendor "CMPS, KhPI" --app-version 1.0.0 --icon src/main/resources/logo.icns \
   --type app-image --dest dist
 
 rm -rf tmp
