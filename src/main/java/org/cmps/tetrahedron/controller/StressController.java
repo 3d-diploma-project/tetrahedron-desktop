28b1b33211e0562bcef73726cade2dce998916b1
Gurgen_49 <avagyangurgen555@gmail.com>
2025-03-07 17:35:03 +0100

    Update StressController.java

--- src/main/java/org/cmps/tetrahedron/controller/StressController.java
+++ src/main/java/org/cmps/tetrahedron/controller/StressController.java
@@ -63,12 +63,11 @@ public class StressController {
         };
     }
 
-    public static float misesStress(double qx, double txy, double tzx, double qy, double tyz, double qz) {
+    private float misesStress(double qx, double txy, double tzx, double qy, double tyz, double qz) {
         return (float) (1 / Math.sqrt(2)
                 * Math.sqrt(Math.pow(qx - qy, 2)
                 + Math.pow(qy - qz, 2)
                 + Math.pow(qz - qx, 2)
                 + 6 * (Math.pow(txy, 2) + Math.pow(tyz, 2) + Math.pow(tzx, 2))));
     }
-
 }
