f3b8896475ced70742c866e609c8e6f21dafc259
VadimST04 <vadikstarchak04@gmail.com>
2025-03-14 17:49:56 +0100

    add try catch to file call

--- src/main/java/org/cmps/tetrahedron/view/RightToolbar.java
+++ src/main/java/org/cmps/tetrahedron/view/RightToolbar.java
@@ -6,6 +6,7 @@ import org.cmps.tetrahedron.controller.FileChooserController;
 import org.cmps.tetrahedron.controller.ModelController;
 import org.cmps.tetrahedron.controller.SceneController;
 import org.cmps.tetrahedron.controller.StressController;
+import org.cmps.tetrahedron.exception.ModelValidationException;
 import org.cmps.tetrahedron.model.CustomCharacteristic;
 import org.cmps.tetrahedron.model.Stress;
 
@@ -48,13 +49,13 @@ public class RightToolbar {
         FileChooser fileChooser = fileChooserController.createFileChooser();
         File file = fileChooser.showOpenDialog(SceneController.getScene().getWindow());
 
-        if (file != null) {
-            fileChooserController.saveLastUsedDirectory(file);
-            try {
+        try {
+            if (file != null) {
+                fileChooserController.saveLastUsedDirectory(file);
                 ModelController.getInstance().applyDisplacements(file);
-            } catch (Exception e) {
-                e.printStackTrace();
             }
+        } catch (ModelValidationException e) {
+            new ErrorDialog("Помилка!", e.getMessage()).show();
         }
     }
 }
