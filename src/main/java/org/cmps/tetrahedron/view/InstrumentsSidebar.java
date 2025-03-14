2ecb2e772cbf39a9ba05aa3dbc09759893775944
VadimST04 <vadikstarchak04@gmail.com>
2025-03-14 18:05:56 +0100

    add functional to fill the model

--- src/main/java/org/cmps/tetrahedron/view/InstrumentsSidebar.java
+++ src/main/java/org/cmps/tetrahedron/view/InstrumentsSidebar.java
@@ -1,9 +1,19 @@
 package org.cmps.tetrahedron.view;
 
+import javafx.scene.Scene;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import org.cmps.tetrahedron.controller.ModelController;
+import org.cmps.tetrahedron.controller.MouseController;
 import org.cmps.tetrahedron.utils.ResourceReader;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InstrumentsSidebar extends VBox {
 
@@ -19,13 +29,53 @@ public class InstrumentsSidebar extends VBox {
     public InstrumentsSidebar() {
         getStyleClass().add("sidebar");
 
-        String[] iconsFilePaths = {"/icon/cursor.png", "/icon/move.png", "/icon/reload.png", "/icon/scale.png",
-                "/icon/copy.png", "/icon/delete.png"};
+        String[] iconsFilePaths = {"/icon/cursor.png", "/icon/upDown.png", "/icon/leftRight.png", "/icon/img.png",
+                "/icon/filling.png", "/icon/delete.png"};
 
+        int index = 0;
         for (String iconFilePath : iconsFilePaths) {
             Button customSidebarBtn = customButtonWithImage(iconFilePath);
+
+            switch (index) {
+                case 4:
+                    customSidebarBtn.setOnAction(event -> openColorPicker());
+                    break;
+            }
+
             getChildren().add(customSidebarBtn);
+            index++;
         }
     }
+
+    // TODO: Design to ColorPicker
+    private void openColorPicker() {
+        Stage stage = new Stage();
+        ColorPicker colorPicker = new ColorPicker();
+        colorPicker.setOnAction(event -> {
+            Color selectedColor = colorPicker.getValue();
+
+            float[] colorArray = new float[]{
+                    (float) selectedColor.getRed(),
+                    (float) selectedColor.getGreen(),
+                    (float) selectedColor.getBlue()
+            };
+
+            int faceCount = ModelController.getInstance().getFaces().size();
+            List<float[]> colorsList = new ArrayList<>();
+            for (int i = 0; i < faceCount; i++) {
+                colorsList.add(colorArray);
+            }
+
+            ModelController.getInstance().setModelColors(colorsList);
+
+            stage.close();
+        });
+
+        StackPane root = new StackPane(colorPicker);
+        Scene scene = new Scene(root, 100, 40);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
 
