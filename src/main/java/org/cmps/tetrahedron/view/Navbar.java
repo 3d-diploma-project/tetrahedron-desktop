5521a0c996e64f1ffb1aa36d2d944bc7a4e61401
Gurgen_49 <avagyangurgen555@gmail.com>
2025-03-31 16:16:32 +0200

    update design language selector (not completed)

--- src/main/java/org/cmps/tetrahedron/view/Navbar.java
+++ src/main/java/org/cmps/tetrahedron/view/Navbar.java
@@ -2,14 +2,20 @@ package org.cmps.tetrahedron.view;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.SVGPath;
 import org.cmps.tetrahedron.controller.LocalizationController;
 
+import java.io.IOException;
 import java.util.Locale;
 import java.util.prefs.Preferences;
 
@@ -21,6 +27,32 @@ public class Navbar {
     @FXML
     public void initialize() {
         languageSelector.setValue("UA");
+
+        languageSelector.setCellFactory(cb -> new ListCell<>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setGraphic(null);
+                } else {
+                    HBox container = new HBox(20);
+                    Label languageText = new Label(item);
+
+                    SVGPath checkMark = new SVGPath();
+                    checkMark.setContent("M1,5 L3,7 L9,1");
+                    checkMark.setFill(null);
+                    checkMark.setStroke(Color.GREEN);
+                    checkMark.setStrokeWidth(1.5);
+                    checkMark.setVisible(item.equals(languageSelector.getValue()));
+
+                    languageText.setStyle("-fx-font-size: 14px; -fx-font-weight: bold;");
+
+                    container.getChildren().addAll(languageText, checkMark);
+                    container.setAlignment(Pos.CENTER_LEFT);
+                    setGraphic(container);
+                }
+            }
+        });
     }
 
     @FXML
