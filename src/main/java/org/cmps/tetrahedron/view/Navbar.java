3e83592a579085906a06cabcfbdf7c75b8104017
Gurgen_49 <113838045+GurgenAvagyan@users.noreply.github.com>
2025-04-04 23:12:28 +0200

    TET-255 Update design language selector (not completed) (#60)
    * update design language selector (not completed)
    
    * redesign language selector
    
    * small fixes in design
    
    * Update navbar.css

--- src/main/java/org/cmps/tetrahedron/view/Navbar.java
+++ src/main/java/org/cmps/tetrahedron/view/Navbar.java
@@ -2,14 +2,19 @@ package org.cmps.tetrahedron.view;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.ComboBox;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.SVGPath;
 import org.cmps.tetrahedron.controller.LocalizationController;
+import org.cmps.tetrahedron.utils.ResourceReader;
 
+import java.io.IOException;
 import java.util.Locale;
 import java.util.prefs.Preferences;
 
@@ -21,6 +26,31 @@ public class Navbar {
     @FXML
     public void initialize() {
         languageSelector.setValue("UA");
+
+        languageSelector.setCellFactory(cb -> new ListCell<>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setGraphic(null);
+                } else {
+                    HBox container = new HBox();
+                    Label languageText = new Label(item);
+                    languageText.setStyle("-fx-text-fill: #0E0E0E;");
+
+                    Pane pane = new Pane();
+                    HBox.setHgrow(pane, Priority.ALWAYS);
+                    container.setAlignment(Pos.CENTER_LEFT);
+
+                    SVGPath checkMark = ResourceReader.readComponent("/icon/Checkmark.fxml", SVGPath.class);
+                    HBox.setMargin(checkMark, new Insets(0, 5, 0, 0));
+                    checkMark.setVisible(languageSelector.getValue().equals(item));
+
+                    container.getChildren().addAll(languageText, pane, checkMark);
+                    setGraphic(container);
+                }
+            }
+        });
     }
 
     @FXML
