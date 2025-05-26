36468618572dff3a04bf73c596cb958c00aa0648
Gurgen_49 <avagyangurgen555@gmail.com>
2025-05-26 19:30:01 +0200

    Support grey style for legend

--- src/main/java/org/cmps/tetrahedron/utils/LegendUtils.java
+++ src/main/java/org/cmps/tetrahedron/utils/LegendUtils.java
@@ -1,8 +1,6 @@
 package org.cmps.tetrahedron.utils;
 
-import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.awt.*;
 import java.util.HashMap;
@@ -14,12 +12,7 @@ public class LegendUtils {
     @Getter
     private static int colorArraySize = 7;
 
-    public static Map<Integer, float[]> COLORS = getColors();
-
-    public static void setColorArraySize(int size) {
-        colorArraySize = size;
-        COLORS = getColors();
-    }
+    public static Map<Integer, float[]> COLORS = getHSVColors();
 
     public static TreeMap<Float, Integer> buildLegend(float min, float max) {
         TreeMap<Float, Integer> stressColorMap = new TreeMap<>();
@@ -34,7 +27,7 @@ public class LegendUtils {
         return stressColorMap;
     }
 
-    private static Map<Integer, float[]> getColors() {
+    private static Map<Integer, float[]> getHSVColors() {
         double jump = 0.66 / (colorArraySize * 1.0);
         Map<Integer, float[]> colors = new HashMap<>();
 
@@ -44,5 +37,23 @@ public class LegendUtils {
         }
 
         return colors;
+    }
+
+    private static Map<Integer, float[]> getGrayColors() {
+        Map<Integer, float[]> colors = new HashMap<>();
+        float minGray = 0.2f;
+        float maxGray = 0.8f;
+
+        for (int i = 0; i < colorArraySize; i++) {
+            float gray = minGray + (maxGray - minGray) * (i / (float) (colorArraySize - 1));
+            colors.put(i, new float[]{gray, gray, gray});
+        }
+
+        return colors;
+    }
+
+    public static void setColorArraySizeAndTheme(int size, boolean grayscale) {
+        colorArraySize = size;
+        COLORS = grayscale ? getGrayColors() : getHSVColors();
     }
 }
--- src/main/java/org/cmps/tetrahedron/view/ColorPickerComponent.java
+++ src/main/java/org/cmps/tetrahedron/view/ColorPickerComponent.java
@@ -1,17 +1,11 @@
 package org.cmps.tetrahedron.view;
 
-import javafx.application.Platform;
-import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import lombok.Getter;
 import org.cmps.tetrahedron.controller.LocalizationController;
-import org.cmps.tetrahedron.controller.ModelController;
 import org.cmps.tetrahedron.controller.StressController;
 import org.cmps.tetrahedron.enums.StressDisplayOption;
 import org.cmps.tetrahedron.exception.ModelValidationException;
@@ -19,8 +13,8 @@ import org.cmps.tetrahedron.model.ColorSettings;
 import org.cmps.tetrahedron.utils.DialogUtils;
 import org.cmps.tetrahedron.utils.LegendUtils;
 import org.cmps.tetrahedron.utils.ResourceReader;
+import org.cmps.tetrahedron.view.component.Switch;
 
-import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -29,6 +23,7 @@ public class ColorPickerComponent {
     private static final LocalizationController local = LocalizationController.getInstance();
 
     private Color tempSelectedColor;
+    private boolean isLegendThemeGrayscale = false;
 
     @FXML
     public Button pickColor;
@@ -46,14 +41,30 @@ public class ColorPickerComponent {
     public Button saveButton;
 
     @FXML
+    private Switch elementGridController;
+
+    @FXML
     public void initialize() {
         float[] colorArray = ColorSettings.getInstance().getModelColor();
         tempSelectedColor = Color.color(colorArray[0], colorArray[1], colorArray[2]);
 
         colorPicker.setValue(tempSelectedColor);
         updateColorDisplay(tempSelectedColor);
-
         legendCountInput.setText(String.valueOf(LegendUtils.getColorArraySize()));
+
+        isLegendThemeGrayscale = ColorSettings.getInstance().isGrayscaleLegendTheme();
+
+        elementGridController.setInitialState(
+                local.getString(LocalizationController.COLOR_PICKER_BUNDLE, "legend-theme"),
+                isLegendThemeGrayscale,
+                this::onSwitchToggle
+        );
+        elementGridController.setLabelStyle("-fx-font-family: 'Geologica Roman'; -fx-font-size: 13px; -fx-text-fill: #0E0E0E;");
+    }
+
+    private void onSwitchToggle(boolean isOn) {
+        isLegendThemeGrayscale = isOn;
+        ColorSettings.getInstance().setGrayscaleLegendTheme(isLegendThemeGrayscale);
     }
 
     private void updateColorDisplay(Color color) {
@@ -96,20 +107,24 @@ public class ColorPickerComponent {
             ColorSettings.getInstance().setColoredInSelectedColor(true);
         }
 
-        Stage stage = (Stage) saveButton.getScene().getWindow();
-        stage.close();
-
+        int colorCount;
         try {
-            int colorCount = Integer.parseInt(legendCountInput.getText());
-            if (colorCount >= 2 && colorCount <= 15) {
-                LegendUtils.setColorArraySize(colorCount);
-                StressDisplayOption displayOption = StressController.getInstance().getStress().getDisplayOption();
-                StressController.getInstance().processStressData(displayOption);
-            } else {
+            colorCount = Integer.parseInt(legendCountInput.getText());
+            if (colorCount < 2 || colorCount > 15) {
                 new ErrorDialog(new ModelValidationException("legend-range"));
+                return;
             }
         } catch (NumberFormatException e) {
             new ErrorDialog(new ModelValidationException("legend-format"));
+            return;
         }
+
+        LegendUtils.setColorArraySizeAndTheme(colorCount, isLegendThemeGrayscale);
+
+        StressDisplayOption displayOption = StressController.getInstance().getStress().getDisplayOption();
+        StressController.getInstance().processStressData(displayOption);
+
+        Stage stage = (Stage) saveButton.getScene().getWindow();
+        stage.close();
     }
 }
