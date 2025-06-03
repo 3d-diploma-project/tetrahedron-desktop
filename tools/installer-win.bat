8b6419a2acef7784e421c65f7dbb010627023c89
Mykhailo Hroshevyi <misha999777@gmail.com>
2025-06-03 18:05:54 +0200

    Fix installer scripts

--- tools/installer-win.bat
+++ tools/installer-win.bat
@@ -10,7 +10,7 @@ call copy target\tetrahedron-desktop-%version%-SNAPSHOT-jar-with-dependencies.ja
 call jlink --strip-debug --no-man-pages --no-header-files --compress zip-6 ^
     --add-modules java.base,java.scripting,java.desktop,jdk.unsupported,jdk.unsupported.desktop --output tmp\jre
 
-call jpackage --input tmp\app --main-jar Tetrahedron.jar --main-class org.cmps.tetrahedron.Tetrahedron --runtime-image tmp\jre ^
+call jpackage --input tmp\app --main-jar Tetrahedron.jar --main-class org.cmps.tetrahedron.Launcher --runtime-image tmp\jre ^
     --name Tetrahedron --vendor "CMPS, KhPI" --app-version %version% --icon src/main/resources/logo.ico ^
     --type msi --win-dir-chooser --win-menu --win-shortcut --win-shortcut-prompt --dest dist
 
