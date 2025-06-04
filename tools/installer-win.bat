fb73ba692a546e914954a50d3e79de94710fd957
Mykhailo Hroshevyi <misha999777@gmail.com>
2025-06-04 22:58:36 +0200

    Fix jre image

--- tools/installer-win.bat
+++ tools/installer-win.bat
@@ -8,7 +8,7 @@ call mkdir tmp tmp\app
 call copy target\tetrahedron-desktop-%version%-SNAPSHOT-jar-with-dependencies.jar tmp\app\Tetrahedron.jar
 
 call jlink --strip-debug --no-man-pages --no-header-files --compress zip-6 ^
-    --add-modules java.base,java.xml,java.scripting,jdk.unsupported --output tmp\jre
+    --add-modules java.base,java.scripting,java.desktop,jdk.unsupported,jdk.unsupported.desktop --output tmp\jre
 
 call jpackage --input tmp\app --main-jar Tetrahedron.jar --main-class org.cmps.tetrahedron.Launcher --runtime-image tmp\jre ^
     --name Tetrahedron --vendor "CMPS, KhPI" --app-version %version% --icon src/main/resources/logo.ico ^
