53d9b3666ced9eec036287f8410d3c0acec46581
VadimST04 <113991200+VadimST04@users.noreply.github.com>
2025-07-05 15:45:43 +0200

    add possibility to apply deformations for different achsen (#76)
    * add possibility to apply deformations for different achsen
    
    * TET-230 fixed deformation components selection
    
    * TET-230 made selector wider
    
    ---------
    
    Co-authored-by: mborodin <m.borodin.1999@gmail.com>

--- src/main/java/org/cmps/tetrahedron/controller/DeformationController.java
+++ src/main/java/org/cmps/tetrahedron/controller/DeformationController.java
@@ -1,6 +1,7 @@
 package org.cmps.tetrahedron.controller;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.cmps.tetrahedron.exception.ModelValidationException;
 import org.cmps.tetrahedron.model.ColorSettings;
 import org.cmps.tetrahedron.model.Model;
@@ -8,9 +9,7 @@ import org.cmps.tetrahedron.utils.DataReader;
 import org.cmps.tetrahedron.view.LegendView;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class DeformationController {
 
@@ -20,6 +19,8 @@ public class DeformationController {
 
     @Getter
     private float currentScale = DEFAULT_SCALE;
+    @Getter
+    private Set<String> deformationComponents = null;
 
     @Getter
     private static final DeformationController instance = new DeformationController();
@@ -39,28 +40,36 @@ public class DeformationController {
 
         ColorSettings.getInstance().setColoredInSelectedColor(true);
         LegendView.getInstance().setVisible(false);
-        applyDeformationScale(DEFAULT_SCALE);
+        applyDeformationScale(DEFAULT_SCALE, null);
     }
 
-    public void applyDeformationScale(float scale) {
+    public void applyDeformationScale(float scale, Set<String> components) {
         Model model = ModelController.getInstance().getModel();
-        Map<Integer, float[]> originalVertices = model.getOriginalVertices();
+        Map<Integer, float[]> originalVertices = ModelController.getInstance().getModel().getOriginalVertices();
 
-        if (lastAppliedDeformations == null || originalVertices.isEmpty()) {
+        if (lastAppliedDeformations == null || originalVertices == null) {
             return;
         }
 
+        deformationComponents = components;
         currentScale = scale;
+        System.out.println(scale);
 
+        int i = 0;
         Map<Integer, float[]> modelVertices = new HashMap<>();
         for (Integer idx : originalVertices.keySet()) {
             float[] orig = originalVertices.get(idx);
-            float[] def = lastAppliedDeformations.get(idx - 1);
-            modelVertices.put(idx, new float[]{
-                    orig[0] + def[0] * scale,
-                    orig[1] + def[1] * scale,
-                    orig[2] + def[2] * scale
-            });
+            float[] def = lastAppliedDeformations.get(i++);
+            float[] current = new float[] {orig[0], orig[1], orig[2]};
+            modelVertices.put(idx, current);
+
+            for (String component : Optional.ofNullable(deformationComponents).orElse(Set.of("x", "y", "z"))) {
+                switch (component) {
+                    case "x" -> current[0] = orig[0] + def[0] * currentScale;
+                    case "y" -> current[1] = orig[1] + def[1] * currentScale;
+                    case "z" -> current[2] = orig[2] + def[2] * currentScale;
+                }
+            }
         }
 
         model.updateVertices(modelVertices);
--- src/main/java/org/cmps/tetrahedron/controller/LocalizationController.java
+++ src/main/java/org/cmps/tetrahedron/controller/LocalizationController.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.cmps.tetrahedron.i18n.LocalizationListener;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 
 public class LocalizationController {
@@ -22,6 +23,7 @@ public class LocalizationController {
     public static final String WARNING_DIALOG_BUNDLE = "i18n.warning-dialog";
     public static final String MODEL_FILES_PICKER_BUNDLE = "i18n.model-files-picker";
     public static final String STRESS_DIALOG_BUNDLE = "i18n.stress-dialog";
+    public static final String DEFORMATION_DIALOG_BUNDLE = "i18n.deformation-dialog";
     public static final String VERTEX_INFO_BUNDLE = "i18n.vertex-info";
     public static final String COLOR_PICKER_BUNDLE = "i18n.color-picker";
 
@@ -33,15 +35,10 @@ public class LocalizationController {
 
     private void loadBundles() {
         bundles.clear();
-        bundles.put(RIGHT_TOOLBAR_BUNDLE, ResourceBundle.getBundle(RIGHT_TOOLBAR_BUNDLE, currentLocale));
-        bundles.put(INDEX_FILE_SELECTOR_BUNDLE, ResourceBundle.getBundle(INDEX_FILE_SELECTOR_BUNDLE, currentLocale));
-        bundles.put(NODE_FILE_SELECTOR_BUNDLE, ResourceBundle.getBundle(NODE_FILE_SELECTOR_BUNDLE, currentLocale));
-        bundles.put(ERROR_DIALOG_BUNDLE, ResourceBundle.getBundle(ERROR_DIALOG_BUNDLE, currentLocale));
-        bundles.put(WARNING_DIALOG_BUNDLE, ResourceBundle.getBundle(WARNING_DIALOG_BUNDLE, currentLocale));
-        bundles.put(MODEL_FILES_PICKER_BUNDLE, ResourceBundle.getBundle(MODEL_FILES_PICKER_BUNDLE, currentLocale));
-        bundles.put(STRESS_DIALOG_BUNDLE, ResourceBundle.getBundle(STRESS_DIALOG_BUNDLE, currentLocale));
-        bundles.put(VERTEX_INFO_BUNDLE, ResourceBundle.getBundle(VERTEX_INFO_BUNDLE, currentLocale));
-        bundles.put(COLOR_PICKER_BUNDLE, ResourceBundle.getBundle(COLOR_PICKER_BUNDLE, currentLocale));
+        Stream.of(RIGHT_TOOLBAR_BUNDLE, INDEX_FILE_SELECTOR_BUNDLE, NODE_FILE_SELECTOR_BUNDLE,
+                  ERROR_DIALOG_BUNDLE, WARNING_DIALOG_BUNDLE, MODEL_FILES_PICKER_BUNDLE,
+                  STRESS_DIALOG_BUNDLE, VERTEX_INFO_BUNDLE, COLOR_PICKER_BUNDLE, DEFORMATION_DIALOG_BUNDLE)
+              .forEach(bundle -> bundles.put(bundle, ResourceBundle.getBundle(bundle, currentLocale)));
     }
 
     public ResourceBundle getBundle(String name) {
